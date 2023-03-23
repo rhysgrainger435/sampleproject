8a2cc46ad4a47057ae5c491c2342a7926a10f549
rhysgrainger435 <x00162031@mytudublin.ie>
2023-03-23 02:22:52 +0000

    storing messages, have bug that needs fixing

--- room/consumers.py
+++ room/consumers.py
@@ -1,7 +1,10 @@
 import json
 
+from django.contrib.auth.models import User
 from channels.generic.websocket import AsyncWebsocketConsumer
 from asgiref.sync import sync_to_async
+
+from .models import Message, Room
 
 class ChatConsumer(AsyncWebsocketConsumer):
     async def connect(self):
@@ -16,7 +19,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
         await self.accept()
     
-    async def disconnect(self):
+    async def disconnect(self, code):
         await self.channel_layer.group_discard(
             self.room_group_name,
             self.channel_name
@@ -29,7 +32,7 @@ class ChatConsumer(AsyncWebsocketConsumer):
         username = data['username']
         room = data['room']
 
-        #await self.save_message(username, room, message)
+        await self.save_message(username, room, message)
 
         await self.channel_layer.group_send(
             self.room_group_name,
@@ -48,3 +51,10 @@ class ChatConsumer(AsyncWebsocketConsumer):
             'message': message,
             'username': username,
         }))
+    
+    @sync_to_async
+    def save_message(self, username, room, message):
+        user = User.objects.get(username=username)
+        room = Room.objects.get(slug=room)
+
+        Message.objects.create(user=user, room=room, content=message)
--- room/views.py
+++ room/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
-from .models import Room
+from .models import Room, Message
 
 @login_required
 def rooms(request):
@@ -12,5 +12,6 @@ def rooms(request):
 @login_required
 def room(request, slug):
     room = Room.objects.get(slug=slug)
+    messages = Message.objects.filter(room=room)
 
-    return render(request, 'room/room.html', {'room': room})
+    return render(request, 'room/room.html', {'room': room, 'messages': messages})
