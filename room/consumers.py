e34895cae886f4961dd409737868cc6f17a9015b
rhysgrainger435 <x00162031@mytudublin.ie>
2023-03-23 01:52:57 +0000

    error fixed room attribute in chat_message should not have been there

--- room/consumers.py
+++ room/consumers.py
@@ -24,27 +24,27 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     async def receive(self, text_data):
         data = json.loads(text_data)
+        print(data)
         message = data['message']
         username = data['username']
         room = data['room']
 
-        await self.channel_layer.group_Send(
+        #await self.save_message(username, room, message)
+
+        await self.channel_layer.group_send(
             self.room_group_name,
             {
                 'type': 'chat_message',
                 'message': message,
-                'username': username,
-                'room': room
+                'username': username
             }
         )
     
     async def chat_message(self, event):
         message = event['message']
         username = event['username']
-        room = event['room']
 
         await self.send(text_data=json.dumps({
             'message': message,
             'username': username,
-            'room': room
         }))
