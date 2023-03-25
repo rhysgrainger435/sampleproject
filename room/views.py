e0d6ff96be62ccaaf4715c42318ccaa87af611de
rhysgrainger435 <x00162031@mytudublin.ie>
2023-03-25 13:49:34 +0000

    search functionality

--- room/views.py
+++ room/views.py
@@ -6,8 +6,14 @@ from .models import Room, Message
 @login_required
 def rooms(request):
     rooms = Room.objects.all()
+    query = request.GET.get('q')
+    if query:
+        rooms = Room.objects.filter(name__icontains=query)
+    else:
+        rooms = Room.objects.all()
+    context = {'rooms': rooms, 'query': query}
 
-    return render(request, 'room/rooms.html', {'rooms': rooms})
+    return render(request, 'room/rooms.html', context)
 
 @login_required
 def room(request, slug):
