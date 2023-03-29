098f7f01ad1742871e1663f58376c8cf8b50a33a
rhysgrainger435 <x00162031@mytudublin.ie>
2023-03-29 01:33:06 +0100

    adding test case for consumer file

--- room/test.py
+++ room/test.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
+from channels.testing import WebsocketCommunicator
 from django.contrib.auth.models import User
 from .models import Room, Message
+from .consumers import Room, Message, ChatConsumer
 
 class MessageModelTestCase(TestCase):
     def setUp(self):
@@ -45,3 +47,55 @@ class MessageModelTestCase(TestCase):
         self.assertEqual(messages[0], message1)
         self.assertEqual(messages[1], message2)
         self.assertEqual(messages[2], message3)
+
+class ChatConsumerTestCase(TestCase):
+
+    async def connect(self, room_name):
+        communicator = WebsocketCommunicator(ChatConsumer.as_asgi(), f'/ws/chat/{room_name}/')
+        connected, _ = await communicator.connect()
+        self.assertTrue(connected)
+        return communicator
+
+    async def disconnect(self, communicator):
+        await communicator.disconnect()
+
+    async def send_message(self, communicator, message, username, room):
+        await communicator.send_json_to({
+            'message': message,
+            'username': username,
+            'room': room,
+        })
+
+    async def receive_message(self, communicator):
+        message = await communicator.receive_json_from()
+        self.assertIn('message', message)
+        self.assertIn('username', message)
+        return message
+
+    async def test_chat_consumer(self):
+        user = User.objects.create_user(username='testuser', password='testpass')
+        room = Room.objects.create(name='testroom', slug='testroom')
+        message = 'Test message'
+        communicator1 = await self.connect(room.slug)
+        communicator2 = await self.connect(room.slug)
+
+        # Send message from first user
+        await self.send_message(communicator1, message, user.username, room.slug)
+        response = await self.receive_message(communicator1)
+        self.assertEqual(response['message'], message)
+
+        # Send message from second user
+        await self.send_message(communicator2, message, user.username, room.slug)
+        response = await self.receive_message(communicator1)
+        self.assertEqual(response['message'], message)
+        response = await self.receive_message(communicator2)
+        self.assertEqual(response['message'], message)
+
+        # Check if message was saved
+        saved_message = Message.objects.last()
+        self.assertEqual(saved_message.content, message)
+        self.assertEqual(saved_message.user, user)
+        self.assertEqual(saved_message.room, room)
+
+        await self.disconnect(communicator1)
+        await self.disconnect(communicator2)
